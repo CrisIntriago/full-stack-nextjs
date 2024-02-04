f922f80345e7cf73f5f871ed9637b0b411fd6109
CrisIntriago <caintria@espol.edu.ec>
2024-02-04 13:50:33 -0500

    Working

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import styles from "./footer.module.css"
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}> LamaDev</div>
-        <div> LamaDev creative thoughts agency. All rights reserved</div>
+      <div className={styles.logo}>lamadev</div>
+      <div className={styles.text}>
+        Lama creative thoughts agency © All rights reserved.
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -6,15 +6,15 @@ const PostCard = ({post}) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-       <div className={styles.imgContainer}>
-          <Image src="https://images.pexels.com/photos/14983436/pexels-photo-14983436/free-photo-of-italia-catedral-iconico-florencia.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img}/>
-        </div>
-        <span className={styles.date}>21/08/2023</span>
+        {post.img && <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img}/>
+        </div>}
+        <span className={styles.date}>{post.createdAt?.toString().slice(4, 16)}</span>
       </div>
       <div className={styles.bottom}>
         <h1 className={styles.title}>{post.title}</h1>
         <p className={styles.desc}>{post.body}</p>
-        <Link className={styles.link} href={`/blog/${post.id}`}>READ MORE</Link>
+        <Link className={styles.link} href={`/blog/${post.slug}`}>READ MORE</Link>
       </div>
     </div>
   )
