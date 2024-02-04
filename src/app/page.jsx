2a0a37aa7d51706b812f84e4850b4370431ea5c3
CrisIntriago <caintria@espol.edu.ec>
2024-02-04 14:20:07 -0500

    Apostrophe error LOL

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,18 +6,19 @@ const Home = () => {
     <div className={styles.container}>
       <div className={styles.textContainer}>
         <h1 className={styles.title}>Social Media Agency.</h1>
-        <p className={styles.desc}>Boost your Business with Unmatched Social Media Presence.
-        Let Us Elevate your Brand's Voice and Connect with your Customers.</p>
+        <p className={styles.desc}>
+          Boost your Business with Unmatched Social Media Presence.
+          Let Us Elevate your Brand&apos;s Voice and Connect with your Customers.</p>
         <div className={styles.buttons}>
           <button className={styles.button}>Learn More</button>
           <button className={styles.button}>Contact</button>
         </div>
         <div className={styles.brands}>
-          <Image src="/brands.png" alt="" fill className={styles.brandImg}/>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
         </div>
       </div>
       <div className={styles.imgContainer}>
-        <Image src="/hero.gif" alt="" fill className={styles.heroImg}/>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
       </div>
     </div>
   );
